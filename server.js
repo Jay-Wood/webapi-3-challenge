ff9eb2eb5e6d4c69dce52c6e4ca613004e40a3bb
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-04 16:51:37 -0700

    Logger function working

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+server.use(express.json());
+server.use(logger);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
 //custom middleware
-
 function logger(req, res, next) {
-
+    console.log("LOGGER: ", req.method, req.url, Date.now())
+    next();
 };
+
+
 
 module.exports = server;
